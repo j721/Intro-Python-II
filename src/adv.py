9dab7b361828efbf48f4fc1d798229631ec64fbc
Julia Vu <juliadvu@gmail.com>
2020-06-04 17:35:52 -0500

    created items and working on add and drop item method

--- src/adv.py
+++ src/adv.py
@@ -39,19 +39,20 @@ room['treasure'].s_to = room['narrow']
 # Main
 
 items ={
-    "cupcake": Item("Cupcake Power Booster", "This is used to make you stronger")
+    "cupcake": Item("Cupcake Power Booster", "This is used to make you stronger"),
+    "night vision googles": Item("Night vision googles", "This will help you to see if the dark."),
+    "light saber": Item("light saber", "May the force be with you.")
 }
 
 room['outside'].items.append(items["cupcake"])
+room['narrow'].items.append(items["night vision googles"])
+room['foyer'].items.append(items["light saber"])
 
 # Make a new player object that is currently in the 'outside' room.
 new_player = Player('Wizard', room['outside'])
-#print(f"Greetings, {player.name}")
-# print('Player is in the:', player.current_room.name)
-
-# def change_room():
-#     new_player.move(selection[0])
-#     if getattr(new_player.current_room, f"{selection}_to") is None:
+print(f"Greetings, {new_player.name}")
+print('Player is in the:', new_player.current_room.name)
+print(f"You are seeing {new_player.current_room.description}")
 
 
 running = True
@@ -63,33 +64,46 @@ while running:
     # print(f"You are seeing {new_player.current_room.description}")
 
 # Waits for user input and decides what to do.
-    selection = input("What do you want to do?\n n for north, e for east, w for west, s for south. q for quit game.)")
+    selection = input("What do you want to do?\n n for north, e for east, w for west, s for south. q for quit game.\n Or enter in a command to pick up or drop item)")
     user_selection = selection.lower().split(" ")
+
 # If the user enters a cardinal direction, attempt to move to the room there.
-    if selection == "q":
-        print("Until next time...exiting the game")
-        break
+    if len(user_selection) ==1:  
+        if selection == "q":
+            print("Until next time...exiting the game")
+            break
+            
+        elif selection == "n" or selection == "s" or selection == "e" or selection == "w":
+            # print("f {selection} made it to selection ")
+            new_player.move(selection)
+            print(f"\n {new_player.name} is in {new_player.current_room.name}\n {new_player.current_room.description}\n\n {new_player.current_room.list_items()}")
         
-    elif selection == "n" or selection == "s" or selection == "e" or selection == "w":
-        # print("f {selection} made it to selection ")
-        new_player.move(selection)
-        print(f"\n {new_player.name} is in {new_player.current_room.name}\n {new_player.current_room.description}\n\n List of items: {new_player.current_room.list_items()}")
-    
-    elif selection == "i":
-        new_player.print_items()
-    
-    else:
-        print("Sorry, command is not recognized.")
+        elif selection == "i":
+            new_player.print_items()
+        
+        else:
+            print("Sorry, command is not recognized.")
 
-    # elif len(user_selection) ==2:
-    #     if user_selection[0] in ["get", "pickup"]:
-    #         if new_player.items [user_selection[1]]:
-    #             new_player.pickup_item(new_player.items[user_selection[1]])
+    elif len(user_selection) ==2:
+        if user_selection[0] in ["get", "pickup"]:
+            if new_player.items [user_selection[1]]:
+                new_player.pickup_item(new_player.items[user_selection[1]])
 
-    #             print("\n\n You have now added a new item into your listed inventory!")
-    #             print(f"{new_player.name} is in {new_player.current_room}\n")
-    #             print(new_player.pickup_item(items))
-    
+                print("\n\n You have now added a new item into your listed inventory!")
+                print(f"{new_player.name} is in {new_player.current_room}\n")
+                print(new_player.current_room.list_items())
+            else:
+                print("That isn't an item.")
+        elif user_selection[0] == "drop":
+            if items[user_selection[1]]:
+                new_player.drop_item(items[user_selection[1]])
+                print("You have dropped an item!")
+                print(new_player.print_items())
+                print(f"{new_player.name} is in {new_player.current_room} \n")
+            else:
+                print("That item does not exist.")
+        else:
+            print("That command is not valid.")
 
 # Print an error message if the movement isn't allowed.
 # If the user enters "q", quit the game.
